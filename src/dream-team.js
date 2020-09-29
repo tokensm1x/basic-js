f2df6533531e69055dac62f6518a5f12320d8724
Misha Pateev <tokensmix@gmail.com>
2020-09-29 13:44:34 +0300

    feat: did task dreamteam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members) === false ||
+    typeof (members) === undefined) {
+    return false;
+  } else {
+    let dream = [];
+    for (let i = 0; i < members.length; i++) {
+      if (typeof (members[i]) === 'string' && typeof (members[i]) !== 'number') {
+        dream.push((members[i].trim().split('')[0].toUpperCase()));
+        dream.sort();
+      }
+    }
+    return dream.join('');
+  }
 };
