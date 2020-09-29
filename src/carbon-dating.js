3665dc8d2d378384f12c642188a29ade906224e4
Misha Pateev <tokensmix@gmail.com>
2020-09-29 12:27:45 +0300

    feat: did task carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof (sampleActivity) !== 'string' ||
+    typeof (sampleActivity) !== 'number' ||
+    sampleActivity < 0) {
+    return false;
+  } else {
+    let activity = MODERN_ACTIVITY / sampleActivity;
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    return Math.ceil(Math.log(activity) / k);
+  }
 };
