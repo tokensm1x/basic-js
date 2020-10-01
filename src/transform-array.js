2d288fda102e696689e73c5801de1fd0593ad970
Misha Pateev <tokensmix@gmail.com>
2020-10-01 21:23:18 +0300

    init: start task transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let obj = {
+    deletePrev: '--discard-prev',
+    deleteNext: `--discard-next`,
+    doublePrev: `--double-prev`,
+    doubleNext: `--double-next`,
+  }
+  let newArr = [];
+  if (!Array.isArray(arr)) { throw new Error }
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case obj.deletePrev:
+        arr.splice(i - 1, 2);
+
+      case obj.deleteNext:
+        arr.splice(i, 2);
+
+      case obj.doubleNext:
+        arr.splice(i, 1, arr[i + 1]);
+
+      case obj.doublePrev:
+        arr.splice(i, 1, arr[i - 1]);
+
+    }
+  }
+  return arr;
 };
