73f7de109dffaeca19d85183642b309a2fc1f7ac
Misha Pateev <tokensmix@gmail.com>
2020-10-04 22:21:05 +0300

    feat: did task chainlink

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  arr: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) this.arr.push(`( )`);
+    else this.arr.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+    if (typeof (position) !== 'number' || !Number.isInteger(position) || position > this.arr.length - 1) {
+      this.arr = [];
+      throw new Error;
+    } else {
+      this.arr.splice(position - 1, 1);
+      return this;
+    }
+
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
+
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.arr.join('~~');
+    this.arr = [];
+    return result;
   }
+
 };
+
 
 module.exports = chainMaker;
