a16d1a1c0aadbcbbbdd1e784c1f6843b2e67e5a7
Misha Pateev <tokensmix@gmail.com>
2020-10-04 20:45:32 +0300

    feat: did task repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|' }) {
+  let add;
+  let addite;
+  let newStr;
+  let result;
+  if (addition === undefined) {
+    newStr = (str + separator).repeat(repeatTimes);
+  } else {
+    add = (addition + additionSeparator).repeat(additionRepeatTimes);
+    addite = add.slice(0, add.length - additionSeparator.length);
+    newStr = (str + addite + separator).repeat(repeatTimes);
+  }
+  result = newStr.slice(0, newStr.length - separator.length);
+
+  return result;
 };
-  
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,4 +13,4 @@ module.exports = class DepthCalculator {
     }
     return count;
   }
-  };
+};
